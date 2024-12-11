709e6abddd50cc87fb16fcb17855f7d5326d7926
Gabriel Eduardo Farinha <94718937+GabrielEduardoFarinha@users.noreply.github.com>
2024-12-11 19:23:40 -0300

    branch do desafio da aula 3

--- app.js
+++ app.js
@@ -1,50 +1,61 @@
-let numeroSecreto = gerarNumeroAleatorio();
-var tentativas =1;
-
-function exibirTextoNaTela(tag, texto){
-    let campo = document.querySelector(tag);
-    campo.innerHTML = texto;
-
-}
-function exibirMensagemInicial(){
-    exibirTextoNaTela('h1' , 'Secret number game');
-    exibirTextoNaTela('p', 'select a number between 1 and 10');
+function calculateImc(){
+    var pWeight = (parseInt(prompt('What is your weight (in kg)?')));
+    var pHeight = (parseFloat(prompt('What is your height (in meters,1.(..))?')));
+    var trans = (pHeight * pHeight);
+    var IMC = (pWeight / trans);
+    var IMCFormatted = IMC.toFixed(2);
+    alert('Your BMI is '+IMCFormatted);
+    
 };
-exibirMensagemInicial();
+calculateImc();
 
-function verificarChute() {
-    let chute = document.querySelector('input').value;
-    if(chute == numeroSecreto){
-        exibirTextoNaTela('h1', 'Good hit!');
-        let palavraTentativa = tentativas > 1 ? 'tries' : 'try';
-        let mensagemTentativas = `you discorver the secret number with ${tentativas} ${palavraTentativa}`;
-        exibirTextoNaTela('p', mensagemTentativas);
-        document.getElementById('reiniciar').removeAttribute('disabled');
-    }else{
-        if(chute > numeroSecreto){
-            exibirTextoNaTela('p', 'number is small');
-        }else{
-            exibirTextoNaTela('p', 'number is bigger');
-        }
-        tentativas++;
-        limparCampo();
+function factorial() {
+    var number = parseInt(prompt('What number do you want to calculate the factorial for?'));
+    var fact = 1;
+    for (var i = 1; i <= number; i++) {
+        fact *= i; 
+    }
+
+    alert('The factorial of ' + number + ' is ' + fact);
+};
+factorial();
+
+function exchange(){
+    dollar = parseFloat(prompt('How many dollars would you like to convert?'));
+    change = dollar * 5.95;
+    alert('The converted value in Brazilian real is '+change);
+
+};
+exchange();
+
+function zone(){
+    format = prompt('Is your area rectangular or circular?');
+    if (format == 'rectangular'){
+        var rWidth = (parseFloat(prompt('what is the width?')));
+        var rHeight = (parseFloat(prompt('what is the height?')));
+        var rArea = (rHeight * rWidth);
+        var rPerimeter = (rHeight*2 / rWidth*2);
+        alert('area is ' + rArea+ ' and perimeter is ' +rPerimeter)
+    }else if(format == 'circle'){
+        var pi = 3.14;
+        var cRadius = (parseFloat(prompt('what is the radius')));
+        var cArea = (pi * cRadius ** 2);
+        var cPerimeter = (2 * pi * cRadius);
+        var perimeterFormatted = cPerimeter.toFixed(2);
+        alert('area is ' + cArea+ ' and perimeter is ' +perimeterFormatted);
+    };
+};
+
+zone();
+
+function multiplicationTable(){
+    var numberTable = (parseInt(prompt('which number do you want the multiplication table of?')));
+    var results = "";
+    for(var i=1; i <=10;i++){
+        let tableResult = numberTable * 1;
+        results += `${numberTable} * ${i} = ${tableResult}\n`;
+        alert(results);
     }
 
 }
-
-function gerarNumeroAleatorio(){
-   return parseInt(Math.random()* 10 + 1 );
-};
-
-function limparCampo(){
-    chute = document.querySelector('input');
-    chute.value = '';
-};
-
-function reiniciarJogo(){
-    numeroSecreto = gerarNumeroAleatorio();
-    limparCampo();
-    tentativas = 1;
-    exibirMensagemInicial();
-    document.getElementById('reiniciar').setAttribute('disabled',true);
-};
+multiplicationTable();
