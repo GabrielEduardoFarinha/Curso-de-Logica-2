02f51435889c0ff8be56731507909699469ca325
Gabriel Eduardo Farinha <94718937+GabrielEduardoFarinha@users.noreply.github.com>
2024-12-11 13:13:04 -0300

    Primeiro commit
    Commit inicial contendo os primeiros códigos de manipulação de textos e função

---
+++ app.js
@@ -0,0 +1,9 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Secret number game';
+
+let paragrafo = document.querySelector('p');
+paragrafo.innerHTML = 'select a number between 1 and 10';
+
+function verificarChute(){
+    console.log('funcionou');
+};
