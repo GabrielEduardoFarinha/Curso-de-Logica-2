5015dcc1409de2109e29823ee90488b7cb092c5e
Gabriel Eduardo Farinha <94718937+GabrielEduardoFarinha@users.noreply.github.com>
2024-12-11 14:13:07 -0300

    branch do desafio do curso
    Desafio de criação de atribuição aos botões

--- app.js
+++ app.js
@@ -1,9 +1,31 @@
-let titulo = document.querySelector('h1');
-titulo.innerHTML = 'Secret number game';
+let title = document.querySelector('h1');
+let city;
+var number1
+var number2
+title.innerHTML = 'Hora do Desafio';
 
-let paragrafo = document.querySelector('p');
-paragrafo.innerHTML = 'select a number between 1 and 10';
-
-function verificarChute(){
-    console.log('funcionou');
+function clickConsole(){
+    console.log('O botão foi clicado');
 };
+
+function clickAlert(){
+    alert('Eu amo JS');
+};
+
+function clickPromt(){
+    let city =prompt('digite o nome de uma cidade');
+    alert('Estive em '+city+ ' e lembrei de você');
+
+};
+
+function clickSoma(){
+    let number1 =prompt('digite um valor inteiro');
+    let number2 =prompt('digite outro valor inteiro');
+    number1 = parseInt(number1);
+    number2 = parseInt(number2);
+    alert('os numero escolhidos foram '+number1+ ' e ' +number2);
+    var add = number1 + number2;
+    alert ('o resultado foi ' +add);
+
+};
+
